e1db0a1a242b56437de458f87d16c0fdf5381a3e
VictorMadronaSanz <165796795+VictorMadronaSanz@users.noreply.github.com>
2025-02-25 00:06:03 +0100

    Nuevos Scriptable Objects
    Falta por hacer aun el drag & drop

--- Assets/Scripts/InventorySystem/Inventories/InventorySlot.cs
+++ Assets/Scripts/InventorySystem/Inventories/InventorySlot.cs
@@ -6,10 +6,8 @@ public class InventorySlot : MonoBehaviour, IDropHandler
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void OnDrop(PointerEventData eventData)
     {
-        if (transform.childCount == 0)
-        {
-            //InventorySlot slot = eventData.pointerDrag.GetComponent<InventoryItem>();
-            //InventorySlot.parentAfterDrag = transform;
-        }
+        GameObject dropped = eventData.pointerDrag;
+        //DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+        //draggableItem.parentAfterDrag = transform;
     }
 }
